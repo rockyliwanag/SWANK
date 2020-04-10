a15afd7c3ad58c62a1b6757217dd370cee686499
Rocky Liwanag <studiorockprod@me.com>
2020-04-09 21:30:09 -0700

    Setup userService and tokenService

--- models/user.js
+++ models/user.js
@@ -1,38 +1,46 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt');
+var bcrypt = require("bcrypt");
 var SALT_ROUNDS = 6;
 
-var userSchema = new mongoose.Schema({
+var userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        unique: true
+      type: String,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        lowercase: true,
-        unique: true,
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
     },
-    password: String,
+    password: {
+      type: String,
+      required: true,
+    },
     city: String,
     zipcode: {
-        type: Number,
-        min: 5,
-        max: 5,
-    }
-}, {
-    timestamps: true
+      type: Number,
+      min: 5,
+      max: 5,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) return next();
+
+  bcrypt.hash(user.password, SALT_ROUNDS, function (err, hash) {
+    if (err) return next(err);
+
+    user.password = hash;
+    next();
+  });
 });
 
-userSchema.pre('save', function (next) {
-    const user = this;
-    if (!user.isModified('password')) return next();
-
-    bcrypt.hash(user.password, SALT_ROUNDS, function (err, hash) {
-        if (err) return next(err);
-
-        user.password = hash;
-        next();
-    });
-});
+module.exports = mongoose.model("User", userSchema);
