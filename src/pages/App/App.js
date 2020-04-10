f2ebc64a6dc63c10934c1499dfc155e52f9e1833
Rocky Liwanag <studiorockprod@me.com>
2020-04-10 11:54:28 -0700

    add login

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,57 +1,57 @@
-import React, { Component } from 'react';
-import { Route, Switch, Redirect, NavLink } from 'react-router-dom';
-import userService from '../../utils/userService';
+import React, { Component } from "react";
+import { Route, Switch, Redirect, NavLink } from "react-router-dom";
+import userService from "../../utils/userService";
 // import tokenService from '../../utils/tokenService';
-import './App.css';
+import "./App.css";
 
 /*------ Pages ------*/
 // import SignupPage from '../SignupPage/SignupPage';
-import LoginPage from '../LoginPage/LoginPage';
-
-
+import LoginPage from "../LoginPage/LoginPage";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      user: userService.getUser()
+      user: userService.getUser(),
     };
   }
 
   /*------ Handlers ------*/
   handleSignupOrLogin = () => {
     this.setState({
-      user: userService.getUser()
+      user: userService.getUser(),
     });
-  }
+  };
 
   render() {
     console.log(this.state.user);
-    return ( 
-    <div className = "App" >
-      <header className = "App-header" >
-        <p >
-          HELLO WORLD!
-        </p>
-        <nav>
-        <NavLink to="/" className="Nav-link">HOME</NavLink>
-        <NavLink to="/login" className="Nav-link">LOGIN</NavLink>
-        </nav> 
-      </header>
-      <body className = "App-body">
-        <p>
-          Good Morning!
-        </p>
-      </body>
-      <Switch>
-        <Route exact path='/login' render={({ history }) => 
-          <LoginPage
-            history={history}
-            handleSignupOrLogin={this.handleSignupOrLogin}
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>HELLO WORLD!</p>
+          <nav>
+            <NavLink to="/" className="Nav-link">
+              HOME
+            </NavLink>
+            <NavLink to="/login" className="Nav-link">
+              LOGIN
+            </NavLink>
+          </nav>
+        </header>
+
+        <Switch>
+          <Route
+            exact
+            path="/login"
+            render={({ history }) => (
+              <LoginPage
+                history={history}
+                handleSignupOrLogin={this.handleSignupOrLogin}
+              />
+            )}
           />
-        }/>
-      </Switch>
-    </div>
+        </Switch>
+      </div>
     );
   }
 }
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./LoginPage.css";
+import userService from "../../utils/userService";
+
+class LoginPage extends Component {
+  state = {
+    username: "",
+    pw: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await userService.login(this.state);
+      // Let <App> know a user has signed up!
+      this.props.handleSignupOrLogin();
+      // Successfully signed up
+      this.props.history.push("/");
+    } catch (err) {
+      // Invalid user data (probably duplicate email)
+      alert("Invalid Credentials!");
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <div>
+              <input
+                type="username"
+                className="form-control"
+                placeholder="Username"
+                value={this.state.username}
+                name="username"
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+                value={this.state.pw}
+                name="pw"
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+          <div>
+            <div>
+              <button> Log In </button>&nbsp;&nbsp;&nbsp;
+              <Link to="/">Cancel</Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
