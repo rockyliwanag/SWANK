8d1b7fc05e7da0e7f295cf5e41a7994f0bd13e74
Rocky Liwanag <studiorockprod@me.com>
2020-04-09 22:46:43 -0700

    authentication part 1

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../../models/user');
+var usersCtrl = require('../../controllers/users');
 
+/*---------- Public Routers ----------*/
+router.post('/signup', usersCtrl.signup);
+router.post('/login', usersCtrl.login);
 
+/*---------- Protected Routes ---------*/
 
 
 
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import { Route, Switch, Redirect} from 'react-router-dom';
+import userService from '../../utils/userService';
+import tokenService from '../../utils/tokenService';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*------ Pages ------*/
+
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            HELLO WORLD!
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
